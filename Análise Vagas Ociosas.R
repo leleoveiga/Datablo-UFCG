c6a697912c9b954161b021e54deeea136714ac86
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-06-19 02:24:11 -0300

    Reprovação2002.csv limpo

--- Análise Vagas Ociosas.R
+++ Análise Vagas Ociosas.R
@@ -9,8 +9,6 @@ library(purrr)
 #OBJ: Gráfico de barras, mostrando a quantidade de vagas ociosas por curso
 
 vagasOciosasPorCurso <- read.csv(file = 'vagasOciosasPorCurso.csv')
-View(vagasOciosasPorCurso)
-str(vagasOciosasPorCurso)
 
 #Correção dos Títulos das Colunas:
 names(vagasOciosasPorCurso)[1:10] <- c("Código","Curso","Campus","Alunos Ativos","Vaga 1 Sem","Vaga 2 Sem","Área","Duração Padrão","Fator de Retenção","Vagas Ociosas")
@@ -28,161 +26,150 @@ data.edit1$`Vaga 1 Sem` <- as.numeric(levels(data.edit1$`Vaga 1 Sem`))[data.edit
 data.edit1$`Vaga 2 Sem` <- as.numeric(levels(data.edit1$`Vaga 2 Sem`))[data.edit1$`Vaga 2 Sem`] 
 data.edit1$`Vagas Ociosas` <- as.numeric(levels(data.edit1$`Vagas Ociosas`))[data.edit1$`Vagas Ociosas`] 
 
-#Fundindo Linhas:
-data.edit2 <- data.edit1[c(-1,-47,-95,-2),] #excluindo linhas vazias
-data.edit2$Campus <- as.factor(data.edit2$Campus)
-separate(data.edit2, Curso, sep = "-", into = c("Curso", "Turno")) #não funciona, pesquisar dps
-class(data.edit2$Curso)
+#Fundindo Linhas
+data.edit1 <- data.edit1[c(-1,-47,-95,-2),] #excluindo linhas vazias
 
-data.test <- data.edit2
+#Corrigindo ordem das linhas
+data.edit1 <- arrange(data.edit1, `Curso`, `Campus`)
+
+#Transformando Campus em fatos
+data.edit1$Campus <- as.factor(data.edit1$Campus)
 
 # USAR O STRINGR para manipular as strings de curso e uniformizá-las. em seguida 
 # organizar com tidyr e diplyr
 
-data.test$Turno <- str_extract(data.test$Curso, "\\- [:upper:]") #extrair de turnos e salvar em cursos "- (letramaius)"
-data.test$Turno <- str_sub(data.test$Curso, start = -1) #Pegar apenas o último caractere
-data.test$Turno <- as.factor(data.test$Turno)
-data.test$Curso <- as.character(data.test$Curso)
-
+data.edit1$Turno <- str_extract(data.edit1$Curso, "\\- [:upper:]") #extrair de turnos e salvar em cursos "- (letramaius)"
+data.edit1$Turno <- str_sub(data.edit1$Curso, start = -1) #Pegar apenas o último caractere
+data.edit1$Turno <- as.factor(data.edit1$Turno)
+data.edit1$Curso <- as.character(data.edit1$Curso)
 
 #Tratando Strings:
-levels(data.test$Turno)
 
-curso <- data.test$Curso #criando vetores com as colunas
+data.edit1$Curso <- str_replace_all(data.edit1$Curso, "[-]", "")
+data.edit1$Curso <- str_replace(data.edit1$Curso, "[M]$", "")
+data.edit1$Curso <- str_replace(data.edit1$Curso, "[N]$", "")
+data.edit1$Curso <- str_replace(data.edit1$Curso, "[D]$", "")
+data.edit1$Curso <- str_replace(data.edit1$Curso, "[V]$", "")
 
-curso <- str_replace_all(curso, "[-]", "")
-curso <- str_replace(curso, "[M]$", "")
-curso <- str_replace(curso, "[N]$", "")
-curso <- str_replace(curso, "[D]$", "")
-curso <- str_replace(curso, "[V]$", "")
+data.edit1$Turno <- as.character(data.edit1$Turno)
+data.edit1[76,7]  <- "D"
+data.edit1[79:80,7]  <- "I"
+data.edit1[41,7]  <- "I"
 
-data.test$Curso <- curso #substitui a col cursos pelos valores limpos
-#data.test$Curso <- as.character(data.test$Curso) #foi necessário transformar em character antes de passar para factor
-data.test$Curso <- as.factor(data.test$Curso)
+#data.edit1$Curso <- as.character(data.edit1$Curso) #foi necessário transformar em character antes de passar para factor
+data.edit1$Curso <- as.factor(data.edit1$Curso)
 
 #agrupando linhas de mesmo nome
 #primeiro dividir em dataframes menores e então juntá-los
-data.test2 <- data.test
 
-total_vagas1 <- data.test2 %>% #Criando primeiro fragmento (total de vagas 1)
+total_vagas1 <- data.edit1 %>% #Criando primeiro fragmento (total de vagas 1)
   group_by(Curso) %>% 
-  summarise(Total_Vagas1 = sum(`Vaga 1 Sem`)) #Digitar sum(data.test2$'Vaga 1 Sem)
+  summarise(Total_Vagas1 = sum(`Vaga 1 Sem`)) #Digitar sum(data.edit1$'Vaga 1 Sem)
 total_vagas1 <- as.data.frame(total_vagas1)
 
-total_vagas2 <- data.test2 %>% #criando segundo fragmento (total vagas 2)
+total_vagas2 <- data.edit1 %>% #criando segundo fragmento (total vagas 2)
   group_by(Curso) %>% 
   summarise(Total_Vagas2 = sum(`Vaga 2 Sem`)) 
 total_vagas2 <- as.data.frame(total_vagas2)
 
-alunos_ativos <- data.test2 %>% #criando 3º fragmento (Alunos Ativos)
+alunos_ativos <- data.edit1 %>% #criando 3º fragmento (Alunos Ativos)
   group_by(Curso) %>% 
   summarise(Alunos_Ativos = sum(`Alunos Ativos`)) 
 alunos_ativos <- as.data.frame(alunos_ativos)
 
-vagas_ociosas <- data.test2 %>% #Criando 4º fragmento (Vagas Ociosas)
+vagas_ociosas <- data.edit1 %>% #Criando 4º fragmento (Vagas Ociosas)
   group_by(Curso) %>% 
   summarise(Vagas_Ociosas = sum(`Vagas Ociosas`)) 
 vagas_ociosas <- as.data.frame(vagas_ociosas)
 
-#Finalmente, juntando os fragmentos na construção da tabela final
-Final <- inner_join(total_vagas1, total_vagas2, by = "Curso")
-Final <- inner_join(Final, vagas_ociosas, by = "Curso")  
-Final <- inner_join(Final, alunos_ativos, by = "Curso")  
-###################
+#Finalmente, juntando os fragmentos na construção da tabela Final.Separado
+Final.Separado <- inner_join(total_vagas1, total_vagas2, by = "Curso")
+Final.Separado <- inner_join(Final.Separado, vagas_ociosas, by = "Curso")  
+Final.Separado <- inner_join(Final.Separado, alunos_ativos, by = "Curso")  
+
+names(Final.Separado)[2:5] <- c("Vagas 1º Sem","Vagas 2º Sem","Vagas Ociosas","Alunos Ativos")
+
 #Criando agrupamentos por curso
-total_vagas <- data.test2 %>% #Criando primeiro fragmento (total de vagas 1)
+total_vagas <- data.edit1 %>% #Criando primeiro fragmento (total de vagas 1 e total vagas 2)
   group_by(Curso) %>% 
-  summarise(Total_Vagas = sum(`Vaga 1 Sem`, `Vaga 2 Sem`)) #Digitar sum(data.test2$'Vaga 1 Sem)
+  summarise(Total_Vagas = sum(`Vaga 1 Sem`, `Vaga 2 Sem`))
 total_vagas <- as.data.frame(total_vagas)
 
-#total_vagas2 <- data.test2 %>% #criando segundo fragmento (total vagas 2)
-#  group_by(Curso) %>% 
-#  summarise(Total_Vagas2 = sum(`Vaga 2 Sem`)) 
-#total_vagas2 <- as.data.frame(total_vagas2)
+#Finalmente, juntando os fragmentos na construção da tabela Final.Total
+Final.Total <- inner_join(total_vagas, vagas_ociosas , by = "Curso")
+Final.Total <- inner_join(Final.Total, alunos_ativos, by = "Curso")  
 
-alunos_ativos <- data.test2 %>% #criando 3º fragmento (Alunos Ativos)
-  group_by(Curso) %>% 
-  summarise(Alunos_Ativos = sum(`Alunos Ativos`)) 
-alunos_ativos <- as.data.frame(alunos_ativos)
+names(Final.Total)[2:4] <- c("Total Vagas","Vagas Ociosas","Alunos Ativos")
 
-vagas_ociosas <- data.test2 %>% #Criando 4º fragmento (Vagas Ociosas)
-  group_by(Curso) %>% 
-  summarise(Vagas_Ociosas = sum(`Vagas Ociosas`)) 
-vagas_ociosas <- as.data.frame(vagas_ociosas)
-
-#Finalmente, juntando os fragmentos na construção da tabela final
-Final <- inner_join(total_vagas, vagas_ociosas , by = "Curso")
-#Final <- inner_join(Final, vagas_ociosas, by = "Curso")  
-Final <- inner_join(Final, alunos_ativos, by = "Curso")  
-
-#Plotando Gráficos FINAL:
-Curso <- Final$Curso
-Curso <- as.character(Curso)
-"Alunos Ativos" <- Final$Alunos_Ativos
-"Vagas Ociosas" <- Final$Vagas_Ociosas
-"Vagas Totais" <- "Alunos Ativos" + "Vagas Ociosas"
-
-names(Final)[2:5] <- c("Vagas 1º Semestre","Vagas 2º Semestre","Vagas Ociosas","Alunos Ativos")
-
-#Agregado <- aggregate(`Alunos Ativos` ~ Curso + `Vagas Ociosas`, data = Final, FUN = sum)
-#ggplot(Agregado, aes(x = `Alunos Ativos`, y = Curso, fill = factor(`Vagas Ociosas`))) +
-#  geom_col(position = "stack")
 ######################
-ggplot(data = Final,
-       mapping = aes(x = `Alunos Ativos`,
+ggplot(data = Final.Total,
+       mapping = aes(x = `Total Vagas`,
                      y = `Vagas Ociosas`,
-                     colour = Curso,)) +
-  geom_point()
-######################
-ggplot(data = Final,
-       mapping = aes(x = `Total_Vagas`,
-                     y = `Vagas_Ociosas`,
-                     size = `Alunos_Ativos`,
+                     size = `Alunos Ativos`,
                      colour = Curso)) +
   scale_size_continuous(range = c(1,15)) +
   xlab("Vagas de Ingressantes(1º e 2º Semestre)") +
   ylab("Vagas Ociosas") +
   geom_point()
 ########################
-ggplot(data = Final,
-       mapping = aes(x = `Total_Vagas`,
-                     y = reorder(Curso, -`Total_Vagas`))) +
+ggplot(data = Final.Total,
+       mapping = aes(x = `Total Vagas`,
+                     y = reorder(`Curso`, -`Total Vagas`))) +
+  geom_col() +
+  xlab("Vagas") +
+  ylab("Curso") +
+  ggtitle("Vagas Disponíveis Anualmente Para Ingressantes por Curso - UFCG")
+########################
+ggplot(data = Final.Total,
+       mapping = aes(x = `Total Vagas`,
+                     y = reorder(Curso, -`Total Vagas`))) +
   geom_col() +
   xlab("Vagas") +
   ylab("Curso") +
   ggtitle("Vagas Disponíveis Anualmente Para Ingressantes por Curso - UFCG")
 ##########################
-ggplot(data = Final,
-       mapping = aes(x = `Alunos_Ativos`,
-                     y = reorder(Curso, -`Alunos_Ativos`))) +
+ggplot(data = Final.Total,
+       mapping = aes(x = `Vagas Ociosas`,
+                     y = reorder(Curso, -`Alunos Ativos`))) +
+  geom_bar(fill = "#08b5ff", mapping = aes(x = `Alunos Ativos`,
+                                 y = reorder(Curso, -`Alunos Ativos`)), stat = "identity") +
+  geom_bar(fill = "#00628c", stat = "identity") +
+  xlab("Vagas Ociosas / Alunos Ativos") +
+  ylab("Curso") +
+  ggtitle("Cursos - UFCG")
+
+##########################
+ggplot(fill = "#08b5ff", data = Final.Total,
+       mapping = aes(fill= `Vagas Ociosas`, x = `Alunos Ativos`,
+                     y = reorder(Curso, -`Alunos Ativos`))) +
+  geom_bar(fill = "#00628c", position="stack", stat = "identity") +
+  xlab("Vagas Ociosas / Alunos Ativos") +
+  ylab("Curso") +
+  ggtitle("Cursos - UFCG")
+##########################
+ggplot(data = Final.Total,
+       mapping = aes(x = `Alunos Ativos`,
+                     y = reorder(Curso, -`Alunos Ativos`))) +
   geom_col() +
   xlab("Alunos Ativos") +
   ylab("Curso") +
   ggtitle("Alunos Ativos por Curso - UFCG")
 ###########################
-ggplot(data = Final,
-       mapping = aes(x = `Vagas_Ociosas`,
-                     y = reorder(Curso, -`Vagas_Ociosas`))) +
+ggplot(data = Final.Total,
+       mapping = aes(x = `Vagas Ociosas`,
+                     y = reorder(Curso, -`Vagas Ociosas`))) +
   geom_col() +
   xlab("Vagas Ociosas") +
   ylab("Curso") +
   ggtitle("Vagas Ociosas por Curso - UFCG")
+
 #Criando agrupamentos por Turno
-Turnos <- data.test2
-Turnos$Turno
-Turnos[76,7] <- "D"
-Turnos[79:80,7] <- "I"
-Turnos[41,7] <- "I"
+Turnos <- data.edit1
 
 total_vagas <- Turnos %>% 
   group_by(Turno) %>% 
   summarise(`Vagas Totais` = sum(`Vaga 1 Sem`, `Vaga 2 Sem`))
 total_vagas <- as.data.frame(total_vagas)
-
-#total_vagas2 <- Turnos %>% 
-#  group_by(Turno) %>% 
-#  summarise(`Vagas 2º Sem` = sum(`Vaga 2 Sem`))
-#total_vagas2 <- as.data.frame(total_vagas2)
 
 alunos_ativos <- Turnos %>% 
   group_by(Turno) %>% 
@@ -211,20 +198,12 @@ ggplot(data = Turnos2,
   ggtitle("Alunos Ativos por Turnos - UFCG")
 ###########
 ggplot(data = Turnos2,
-       mapping = aes(x = reorder(Turno, -`Vagas 1º Sem`),
-                     y = `Vagas 1º Sem`)) +
+       mapping = aes(x = reorder(Turno, -`Vagas Totais`),
+                     y = `Vagas Totais`)) +
   xlab("Turnos") +
   ylab("Vagas 1º Semestre") +
   geom_col(fill = "darkblue") +
-  ggtitle("Vagas Para Ingressantes 1º Semestre por Turno - UFCG")
-################
-ggplot(data = Turnos2,
-       mapping = aes(x = reorder(Turno, -`Vagas 2º Sem`),
-                     y = `Vagas 2º Sem`)) +
-  xlab("Turnos") +
-  ylab("Vagas 2º Semestre") +
-  geom_col(fill = "darkblue") +
-  ggtitle("Vagas Para Ingressantes 2º Semestre por Turno - UFCG")
+  ggtitle("Vagas Totais Para Ingressantes por Turno - UFCG")
 #############
 ggplot(data = Turnos2,
        mapping = aes(x = reorder(Turno, -`Vagas Ociosas`),
@@ -246,17 +225,12 @@ ggplot(data = Turnos2,
   geom_point()
 
 #Criando agrupamentos por campus
-Campus <- data.test2
+Campus <- data.edit1
 
 total_vagas <- Campus %>% 
   group_by(Campus) %>% 
   summarise(`Vagas Totais` = sum(`Vaga 1 Sem`, `Vaga 2 Sem`))
 total_vagas <- as.data.frame(total_vagas)
-
-#total_vagas2 <- Campus %>%     #retrabalhado para plots de dispersão
-#  group_by(Campus) %>% 
-#  summarise(`Vagas 2º Sem` = sum(`Vaga 2 Sem`))
-#total_vagas2 <- as.data.frame(total_vagas2)
 
 alunos_ativos <- Campus %>% 
   group_by(Campus) %>% 
@@ -271,10 +245,6 @@ vagas_ociosas <- as.data.frame(vagas_ociosas)
 #Juntando Fragmentos criados (Retrabalhado para plots de dispersão)
 Campi <- inner_join(total_vagas, alunos_ativos , by = "Campus")
 Campi <- inner_join(Campi, vagas_ociosas, by = "Campus")
-
-#Campi <- inner_join(total_vagas1, total_vagas2, by = "Campus")
-#Campi <- inner_join(Campi, alunos_ativos, by = "Campus")
-#Campi <- inner_join(Campi, vagas_ociosas, by = "Campus")
 
 #Plotando gráficos Campi
 ggplot(data = Campi,
@@ -294,12 +264,12 @@ ggplot(data = Campi,
   ggtitle("Alunos Ativos por Campus - UFCG")
 ###########################
 ggplot(data = Campi,
-       mapping = aes(x = reorder(Campus, -`Vagas 2º Sem`),
-                     y = `Vagas 2º Sem`)) +
+       mapping = aes(x = reorder(Campus, -`Vagas Totais`),
+                     y = `Vagas Totais`)) +
   xlab("Campus") +
-  ylab("Vagas 2º Semestre") +
+  ylab("Vagas Totais") +
   geom_col(fill = "darkblue") +
-  ggtitle("Vagas Para Ingressantes 2º Semestre por Campus - UFCG")
+  ggtitle("Vagas Totais Para Ingressantes por Campus - UFCG")
 #########################
 ggplot(data = Campi,
        mapping = aes(x = reorder(Campus, -`Vagas 1º Sem`),
@@ -319,4 +289,3 @@ ggplot(data = Campi,
   ggtitle("Vagas Totais Para Ingressantes x Vagas Ociosas x Alunos Ativos") +
   scale_size_continuous(range = c(1,15)) +
   geom_point()
-
---
+++ Reprovação_02.R
@@ -0,0 +1,57 @@
+library(stringr)
+library(dbplyr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(purrr)
+library(tibble)
+
+dfPadrao <- read.csv(file = "taxaReprovacaoAnoPorCurso2002.csv")
+dfEditado <- read.csv(file = "taxaReprovacaoAnoPorCurso2002.csv")
+
+####Limpeza e organização básica do dataframe####
+names(dfEditado)[1:7] <- c("Curso", "Reprovações.1", "Matrículas.1", "Porcentagem.1"
+                               ,"Reprovações.2", "Matrículas.2", "Porcentagem.2")
+dfEditado$Porcentagem.1 <- NULL
+dfEditado$Porcentagem.2 <- NULL
+dfEditado <- dfEditado[c(-1, -2, -3, -28, -29, -58, -59, -88, -89),]
+dfEditado$Curso <- str_sub(dfEditado$Curso, 12)
+dfEditado <- arrange(dfEditado, `Curso`)
+
+####Convertendo colunas de factor pra numérico####
+dfEditado$Reprovações.1 <- as.numeric(as.character(dfEditado$Reprovações.1))
+dfEditado$Matrículas.1 <- as.numeric(as.character(dfEditado$Matrículas.1))
+dfEditado$Reprovações.2 <- as.numeric(as.character(dfEditado$Reprovações.2))
+dfEditado$Matrículas.2 <- as.numeric(as.character(dfEditado$Matrículas.2))
+
+####Retirando os turnos dos cursos####
+dfEditado$Curso <- str_replace(dfEditado$Curso, "[MNDV]$", "")
+dfEditado$Curso <- str_trim(dfEditado$Curso)
+dfEditado$Curso <- str_replace(dfEditado$Curso, "[\\-]$","")
+dfEditado$Curso <- str_trim(dfEditado$Curso)
+
+####Renomeando os cursos pra depois unificar####
+dfEditado$Curso[1:6] <- str_sub(dfEditado$Curso[1:6], end = 13)
+dfEditado$Curso[9:18] <- str_sub(dfEditado$Curso[9:18], end = 8)
+dfEditado$Curso[23:26] <- str_sub(dfEditado$Curso[23:26], end = 16)
+dfEditado$Curso[36:40] <- str_sub(dfEditado$Curso[36:40], end = 19)
+dfEditado$Curso[44] <- str_sub(dfEditado$Curso[44], end = 6)
+dfEditado$Curso[49:54] <- str_sub(dfEditado$Curso[49:54], end = 8)
+dfEditado$Curso[56:64] <- str_sub(dfEditado$Curso[56:64], end = 6)
+dfEditado$Curso[68:71] <- str_sub(dfEditado$Curso[68:71], end = 10)
+dfEditado$Curso[75:87] <- str_sub(dfEditado$Curso[75:87], end = 9)
+
+####Juntar os dados por agrupamento dos cursos####
+dfAgrupado <- dfEditado %>%
+  group_by(Curso) %>% 
+  summarise(Reprovações.1 = sum(Reprovações.1), Matrículas.1 = sum(Matrículas.1)
+            , Reprovações.2 = sum(Reprovações.2), Matrículas.2 = sum(Matrículas.2))
+dfAgrupado <- as.data.frame(dfAgrupado)
+
+####Criando a coluna de porcentagem baseada no total####
+porcentagem.total.1 = round((dfAgrupado$Reprovações.1 / dfAgrupado$Matrículas.1) * 100, digits = 1)
+dfAgrupado <- add_column(dfAgrupado, Porcentagem.1 = porcentagem.total.1, .after = 3)
+porcentagem.total.2 = round((dfAgrupado$Reprovações.2 / dfAgrupado$Matrículas.2) * 100, digits = 1)
+dfAgrupado <- add_column(dfAgrupado, Porcentagem.2 = porcentagem.total.2, .after = 6)
+
+write.csv(dfAgrupado, file = "Reprovacao2002.csv")
