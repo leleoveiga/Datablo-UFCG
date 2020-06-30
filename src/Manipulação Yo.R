8acc005bb7b2440989b8673549aea91b66d8ad4a
YoYolops <yohanhxh@gmail.com>
2020-06-30 03:55:21 -0300

    Upload de Dados Editados por Ano

--- src/Manipulação Yo.R
+++ src/Manipulação Yo.R
@@ -36,44 +36,44 @@ write_delim(Reprovacao_2013, "Reprovação_2013.csv", delim = ";")
 write_delim(Reprovacao_2015, "Reprovação_2015.csv", delim = ";")
 write_delim(Reprovacao_2017, "Reprovação_2017.csv", delim = ";")
 write_delim(Reprovacao_2019, "Reprovação_2019.csv", delim = ";")
+ # as linhas a seguir foram usadas varias vezes com o $ alterado
+Reprovacao_2002$Reprovações.1 <- NULL
+Reprovacao_2003$Reprovações.1 <- NULL
+Reprovacao_2004$Reprovações.1 <- NULL
+Reprovacao_2005$Reprovações.1 <- NULL
+Reprovacao_2006$Reprovações.1 <- NULL
+Reprovacao_2007$Reprovações.1 <- NULL
+Reprovacao_2008$Reprovações.1 <- NULL
+Reprovacao_2009$Reprovações.1 <- NULL
+Reprovacao_2010$Reprovações.1 <- NULL
+Reprovacao_2011$Reprovações.1 <- NULL
+Reprovacao_2012$Reprovações.1 <- NULL
+Reprovacao_2013$Reprovações.1 <- NULL
+Reprovacao_2014$Reprovações.1 <- NULL
+Reprovacao_2015$Reprovações.1 <- NULL
+Reprovacao_2016$Reprovações.1 <- NULL
+Reprovacao_2017$Reprovações.1 <- NULL
+Reprovacao_2018$Reprovações.1 <- NULL
+Reprovacao_2019$Reprovações.1 <- NULL
 
-Reprovacao_2002$Porcentagem.1 <- NULL
-Reprovacao_2003$Porcentagem.1 <- NULL
-Reprovacao_2004$Porcentagem.1 <- NULL
-Reprovacao_2005$Porcentagem.1 <- NULL
-Reprovacao_2006$Porcentagem.1 <- NULL
-Reprovacao_2007$Porcentagem.1 <- NULL
-Reprovacao_2008$Porcentagem.1 <- NULL
-Reprovacao_2009$Porcentagem.1 <- NULL
-Reprovacao_2010$Porcentagem.1 <- NULL
-Reprovacao_2011$Porcentagem.1 <- NULL
-Reprovacao_2012$Porcentagem.1 <- NULL
-Reprovacao_2013$Porcentagem.1 <- NULL
-Reprovacao_2014$Porcentagem.1 <- NULL
-Reprovacao_2015$Porcentagem.1 <- NULL
-Reprovacao_2016$Porcentagem.1 <- NULL
-Reprovacao_2017$Porcentagem.1 <- NULL
-Reprovacao_2018$Porcentagem.1 <- NULL
-Reprovacao_2019$Porcentagem.1 <- NULL
-
-Reprovacao_2002$Porcentagem.2 <- NULL
-Reprovacao_2003$Porcentagem.2 <- NULL
-Reprovacao_2004$Porcentagem.2 <- NULL
-Reprovacao_2005$Porcentagem.2 <- NULL
-Reprovacao_2006$Porcentagem.2 <- NULL
-Reprovacao_2007$Porcentagem.2 <- NULL
-Reprovacao_2008$Porcentagem.2 <- NULL
-Reprovacao_2009$Porcentagem.2 <- NULL
-Reprovacao_2010$Porcentagem.2 <- NULL
-Reprovacao_2011$Porcentagem.2 <- NULL
-Reprovacao_2012$Porcentagem.2 <- NULL
-Reprovacao_2013$Porcentagem.2 <- NULL
-Reprovacao_2014$Porcentagem.2 <- NULL
-Reprovacao_2015$Porcentagem.2 <- NULL
-Reprovacao_2016$Porcentagem.2 <- NULL
-Reprovacao_2017$Porcentagem.2 <- NULL
-Reprovacao_2018$Porcentagem.2 <- NULL
-Reprovacao_2019$Porcentagem.2 <- NULL
+Reprovacao_2002$Reprovações.2 <- NULL
+Reprovacao_2003$Reprovações.2 <- NULL
+Reprovacao_2004$Reprovações.2 <- NULL
+Reprovacao_2005$Reprovações.2 <- NULL
+Reprovacao_2006$Reprovações.2 <- NULL
+Reprovacao_2007$Reprovações.2 <- NULL
+Reprovacao_2008$Reprovações.2 <- NULL
+Reprovacao_2009$Reprovações.2 <- NULL
+Reprovacao_2010$Reprovações.2 <- NULL
+Reprovacao_2011$Reprovações.2 <- NULL
+Reprovacao_2012$Reprovações.2 <- NULL
+Reprovacao_2013$Reprovações.2 <- NULL
+Reprovacao_2014$Reprovações.2 <- NULL
+Reprovacao_2015$Reprovações.2 <- NULL
+Reprovacao_2016$Reprovações.2 <- NULL
+Reprovacao_2017$Reprovações.2 <- NULL
+Reprovacao_2018$Reprovações.2 <- NULL
+Reprovacao_2019$Reprovações.2 <- NULL
 
 ####Adição de Colunas Fator Ano####
 
@@ -97,24 +97,203 @@ Reprovacao_2018 <- add_column(Reprovacao_2018, Ano = 2018, .after = 7)
 Reprovacao_2019 <- add_column(Reprovacao_2019, Ano = 2019, .after = 7)
 
 
-Reprovacao_2002$Ano <- as.factor(as.numeric(Reprovacao_2002))
-Reprovacao_2003$Ano <- as.factor(as.numeric(Reprovacao_2003))
-Reprovacao_2004$Ano <- as.factor(as.numeric(Reprovacao_2004))
-Reprovacao_2005$Ano <- as.factor(as.numeric(Reprovacao_2005))
-Reprovacao_2006$Ano <- as.factor(as.numeric(Reprovacao_2006))
-Reprovacao_2007$Ano <- as.factor(as.numeric(Reprovacao_2007))
-Reprovacao_2008$Ano <- as.factor(as.numeric(Reprovacao_2008))
-Reprovacao_2009$Ano <- as.factor(as.numeric(Reprovacao_2009))
-Reprovacao_2010$Ano <- as.factor(as.numeric(Reprovacao_2010))
-Reprovacao_2011$Ano <- as.factor(as.numeric(Reprovacao_2011))
-Reprovacao_2012$Ano <- as.factor(as.numeric(Reprovacao_2012))
-Reprovacao_2013$Ano <- as.factor(as.numeric(Reprovacao_2013))
-Reprovacao_2014$Ano <- as.factor(as.numeric(Reprovacao_2014))
-Reprovacao_2015$Ano <- as.factor(as.numeric(Reprovacao_2015))
-Reprovacao_2016$Ano <- as.factor(as.numeric(Reprovacao_2016))
-Reprovacao_2017$Ano <- as.factor(as.numeric(Reprovacao_2017))
-Reprovacao_2018$Ano <- as.factor(as.numeric(Reprovacao_2018))
-Reprovacao_2019$Ano <- as.factor(as.numeric(Reprovacao_2019))
+Reprovacao_2002$Ano <- as.factor(as.numeric(Reprovacao_2002$Ano))
+Reprovacao_2003$Ano <- as.factor(as.numeric(Reprovacao_2003$Ano))
+Reprovacao_2004$Ano <- as.factor(as.numeric(Reprovacao_2004$Ano))
+Reprovacao_2005$Ano <- as.factor(as.numeric(Reprovacao_2005$Ano))
+Reprovacao_2006$Ano <- as.factor(as.numeric(Reprovacao_2006$Ano))
+Reprovacao_2007$Ano <- as.factor(as.numeric(Reprovacao_2007$Ano))
+Reprovacao_2008$Ano <- as.factor(as.numeric(Reprovacao_2008$Ano))
+Reprovacao_2009$Ano <- as.factor(as.numeric(Reprovacao_2009$Ano))
+Reprovacao_2010$Ano <- as.factor(as.numeric(Reprovacao_2010$Ano))
+Reprovacao_2011$Ano <- as.factor(as.numeric(Reprovacao_2011$Ano))
+Reprovacao_2012$Ano <- as.factor(as.numeric(Reprovacao_2012$Ano))
+Reprovacao_2013$Ano <- as.factor(as.numeric(Reprovacao_2013$Ano))
+Reprovacao_2014$Ano <- as.factor(as.numeric(Reprovacao_2014$Ano))
+Reprovacao_2015$Ano <- as.factor(as.numeric(Reprovacao_2015$Ano))
+Reprovacao_2016$Ano <- as.factor(as.numeric(Reprovacao_2016$Ano))
+Reprovacao_2017$Ano <- as.factor(as.numeric(Reprovacao_2017$Ano))
+Reprovacao_2018$Ano <- as.factor(as.numeric(Reprovacao_2018$Ano))
+Reprovacao_2019$Ano <- as.factor(as.numeric(Reprovacao_2019$Ano))
+
+
+Reprovacao_2002$Reprovações <- Reprovacao_2002$Reprovações.1 + Reprovacao_2002$Reprovações.2
+Reprovacao_2003$Reprovações <- Reprovacao_2003$Reprovações.1 + Reprovacao_2003$Reprovações.2
+Reprovacao_2004$Reprovações <- Reprovacao_2004$Reprovações.1 + Reprovacao_2004$Reprovações.2
+Reprovacao_2005$Reprovações <- Reprovacao_2005$Reprovações.1 + Reprovacao_2005$Reprovações.2
+Reprovacao_2006$Reprovações <- Reprovacao_2006$Reprovações.1 + Reprovacao_2006$Reprovações.2
+Reprovacao_2007$Reprovações <- Reprovacao_2007$Reprovações.1 + Reprovacao_2007$Reprovações.2
+Reprovacao_2008$Reprovações <- Reprovacao_2008$Reprovações.1 + Reprovacao_2008$Reprovações.2
+Reprovacao_2009$Reprovações <- Reprovacao_2009$Reprovações.1 + Reprovacao_2009$Reprovações.2
+Reprovacao_2010$Reprovações <- Reprovacao_2010$Reprovações.1 + Reprovacao_2010$Reprovações.2
+Reprovacao_2011$Reprovações <- Reprovacao_2011$Reprovações.1 + Reprovacao_2011$Reprovações.2
+Reprovacao_2012$Reprovações <- Reprovacao_2012$Reprovações.1 + Reprovacao_2012$Reprovações.2
+Reprovacao_2013$Reprovações <- Reprovacao_2013$Reprovações.1 + Reprovacao_2013$Reprovações.2
+Reprovacao_2014$Reprovações <- Reprovacao_2014$Reprovações.1 + Reprovacao_2014$Reprovações.2
+Reprovacao_2015$Reprovações <- Reprovacao_2015$Reprovações.1 + Reprovacao_2015$Reprovações.2
+Reprovacao_2016$Reprovações <- Reprovacao_2016$Reprovações.1 + Reprovacao_2016$Reprovações.2
+Reprovacao_2017$Reprovações <- Reprovacao_2017$Reprovações.1 + Reprovacao_2017$Reprovações.2
+Reprovacao_2018$Reprovações <- Reprovacao_2018$Reprovações.1 + Reprovacao_2018$Reprovações.2
+Reprovacao_2019$Reprovações <- Reprovacao_2019$Reprovações.1 + Reprovacao_2019$Reprovações.2
+
+
+Reprovacao_2002$Matrículas <- Reprovacao_2002$Matrículas.1 + Reprovacao_2002$Matrículas.2
+Reprovacao_2003$Matrículas <- Reprovacao_2003$Matrículas.1 + Reprovacao_2003$Matrículas.2
+Reprovacao_2004$Matrículas <- Reprovacao_2004$Matrículas.1 + Reprovacao_2004$Matrículas.2
+Reprovacao_2005$Matrículas <- Reprovacao_2005$Matrículas.1 + Reprovacao_2005$Matrículas.2
+Reprovacao_2006$Matrículas <- Reprovacao_2006$Matrículas.1 + Reprovacao_2006$Matrículas.2
+Reprovacao_2007$Matrículas <- Reprovacao_2007$Matrículas.1 + Reprovacao_2007$Matrículas.2
+Reprovacao_2008$Matrículas <- Reprovacao_2008$Matrículas.1 + Reprovacao_2008$Matrículas.2
+Reprovacao_2009$Matrículas <- Reprovacao_2009$Matrículas.1 + Reprovacao_2009$Matrículas.2
+Reprovacao_2010$Matrículas <- Reprovacao_2010$Matrículas.1 + Reprovacao_2010$Matrículas.2
+Reprovacao_2011$Matrículas <- Reprovacao_2011$Matrículas.1 + Reprovacao_2011$Matrículas.2
+Reprovacao_2012$Matrículas <- Reprovacao_2012$Matrículas.1 + Reprovacao_2012$Matrículas.2
+Reprovacao_2013$Matrículas <- Reprovacao_2013$Matrículas.1 + Reprovacao_2013$Matrículas.2
+Reprovacao_2014$Matrículas <- Reprovacao_2014$Matrículas.1 + Reprovacao_2014$Matrículas.2
+Reprovacao_2015$Matrículas <- Reprovacao_2015$Matrículas.1 + Reprovacao_2015$Matrículas.2
+Reprovacao_2016$Matrículas <- Reprovacao_2016$Matrículas.1 + Reprovacao_2016$Matrículas.2
+Reprovacao_2017$Matrículas <- Reprovacao_2017$Matrículas.1 + Reprovacao_2017$Matrículas.2
+Reprovacao_2018$Matrículas <- Reprovacao_2018$Matrículas.1 + Reprovacao_2018$Matrículas.2
+Reprovacao_2019$Matrículas <- Reprovacao_2019$Matrículas.1 + Reprovacao_2019$Matrículas.2
+
+
+####Somando colunas de período####
+
+Reprovacao_2002 <- add_column(Reprovacao_2002, Matrículas = NA, .after = 5)
+Reprovacao_2003 <- add_column(Reprovacao_2003, Matrículas = NA, .after = 5)
+Reprovacao_2004 <- add_column(Reprovacao_2004, Matrículas = NA, .after = 5)
+Reprovacao_2005 <- add_column(Reprovacao_2005, Matrículas = NA, .after = 5)
+Reprovacao_2006 <- add_column(Reprovacao_2006, Matrículas = NA, .after = 5)
+Reprovacao_2007 <- add_column(Reprovacao_2007, Matrículas = NA, .after = 5)
+Reprovacao_2008 <- add_column(Reprovacao_2008, Matrículas = NA, .after = 5)
+Reprovacao_2009 <- add_column(Reprovacao_2009, Matrículas = NA, .after = 5)
+Reprovacao_2010 <- add_column(Reprovacao_2010, Matrículas = NA, .after = 5)
+Reprovacao_2011 <- add_column(Reprovacao_2011, Matrículas = NA, .after = 5)
+Reprovacao_2012 <- add_column(Reprovacao_2012, Matrículas = NA, .after = 5)
+Reprovacao_2013 <- add_column(Reprovacao_2013, Matrículas = NA, .after = 5)
+Reprovacao_2014 <- add_column(Reprovacao_2014, Matrículas = NA, .after = 5)
+Reprovacao_2015 <- add_column(Reprovacao_2015, Matrículas = NA, .after = 5)
+Reprovacao_2016 <- add_column(Reprovacao_2016, Matrículas = NA, .after = 5)
+Reprovacao_2017 <- add_column(Reprovacao_2017, Matrículas = NA, .after = 5)
+Reprovacao_2018 <- add_column(Reprovacao_2018, Matrículas = NA, .after = 5)
+Reprovacao_2019 <- add_column(Reprovacao_2019, Matrículas = NA, .after = 5)
+
+Reprovacao_2002 <- add_column(Reprovacao_2002, Reprovações = NA, .after = 6)
+Reprovacao_2003 <- add_column(Reprovacao_2003, Reprovações = NA, .after = 6)
+Reprovacao_2004 <- add_column(Reprovacao_2004, Reprovações = NA, .after = 6)
+Reprovacao_2005 <- add_column(Reprovacao_2005, Reprovações = NA, .after = 6)
+Reprovacao_2006 <- add_column(Reprovacao_2006, Reprovações = NA, .after = 6)
+Reprovacao_2007 <- add_column(Reprovacao_2007, Reprovações = NA, .after = 6)
+Reprovacao_2008 <- add_column(Reprovacao_2008, Reprovações = NA, .after = 6)
+Reprovacao_2009 <- add_column(Reprovacao_2009, Reprovações = NA, .after = 6)
+Reprovacao_2010 <- add_column(Reprovacao_2010, Reprovações = NA, .after = 6)
+Reprovacao_2011 <- add_column(Reprovacao_2011, Reprovações = NA, .after = 6)
+Reprovacao_2012 <- add_column(Reprovacao_2012, Reprovações = NA, .after = 6)
+Reprovacao_2013 <- add_column(Reprovacao_2013, Reprovações = NA, .after = 6)
+Reprovacao_2014 <- add_column(Reprovacao_2014, Reprovações = NA, .after = 6)
+Reprovacao_2015 <- add_column(Reprovacao_2015, Reprovações = NA, .after = 6)
+Reprovacao_2016 <- add_column(Reprovacao_2016, Reprovações = NA, .after = 6)
+Reprovacao_2017 <- add_column(Reprovacao_2017, Reprovações = NA, .after = 6)
+Reprovacao_2018 <- add_column(Reprovacao_2018, Reprovações = NA, .after = 6)
+Reprovacao_2019 <- add_column(Reprovacao_2019, Reprovações = NA, .after = 6)
+
+####Criando Porcentagem Geral
+
+Percentual = round(
+  (Reprovacao_2002$Reprovações / Reprovacao_2002$Matrículas) * 100, digits = 1)
+
+Reprovacao_2002 <- add_column(Reprovacao_2002, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2003$Reprovações / Reprovacao_2003$Matrículas) * 100, digits = 1)
+
+Reprovacao_2003 <- add_column(Reprovacao_2003, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2004$Reprovações / Reprovacao_2004$Matrículas) * 100, digits = 1)
+
+Reprovacao_2004 <- add_column(Reprovacao_2004, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2005$Reprovações / Reprovacao_2005$Matrículas) * 100, digits = 1)
+
+Reprovacao_2005 <- add_column(Reprovacao_2005, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2006$Reprovações / Reprovacao_2006$Matrículas) * 100, digits = 1)
+
+Reprovacao_2006 <- add_column(Reprovacao_2006, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2007$Reprovações / Reprovacao_2007$Matrículas) * 100, digits = 1)
+
+Reprovacao_2007 <- add_column(Reprovacao_2007, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2008$Reprovações / Reprovacao_2008$Matrículas) * 100, digits = 1)
+
+Reprovacao_2008 <- add_column(Reprovacao_2008, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2009$Reprovações / Reprovacao_2009$Matrículas) * 100, digits = 1)
+
+Reprovacao_2009 <- add_column(Reprovacao_2009, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2010$Reprovações / Reprovacao_2010$Matrículas) * 100, digits = 1)
+
+Reprovacao_2010 <- add_column(Reprovacao_2010, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2011$Reprovações / Reprovacao_2011$Matrículas) * 100, digits = 1)
+
+Reprovacao_2011 <- add_column(Reprovacao_2011, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2012$Reprovações / Reprovacao_2012$Matrículas) * 100, digits = 1)
+
+Reprovacao_2012 <- add_column(Reprovacao_2012, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2013$Reprovações / Reprovacao_2013$Matrículas) * 100, digits = 1)
+
+Reprovacao_2013 <- add_column(Reprovacao_2013, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2014$Reprovações / Reprovacao_2014$Matrículas) * 100, digits = 1)
+
+Reprovacao_2014 <- add_column(Reprovacao_2014, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2015$Reprovações / Reprovacao_2015$Matrículas) * 100, digits = 1)
+
+Reprovacao_2015 <- add_column(Reprovacao_2015, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2016$Reprovações / Reprovacao_2016$Matrículas) * 100, digits = 1)
+
+Reprovacao_2016 <- add_column(Reprovacao_2016, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2017$Reprovações / Reprovacao_2017$Matrículas) * 100, digits = 1)
+
+Reprovacao_2017 <- add_column(Reprovacao_2017, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2018$Reprovações / Reprovacao_2018$Matrículas) * 100, digits = 1)
+
+Reprovacao_2018 <- add_column(Reprovacao_2018, Percentual = Percentual, .after = 3)
+
+Percentual = round(
+  (Reprovacao_2019$Reprovações / Reprovacao_2019$Matrículas) * 100, digits = 1)
+
+Reprovacao_2019 <- add_column(Reprovacao_2019, Percentual = Percentual, .after = 3)
+
+
+
+####Criando MegaDf####
+
+
 
 
 
